1609074f0dd6323189d1766bfb7e53e61fec2f7b
IrvanKurniawan <irvankurniawan624@gmail.com>
2026-05-12 09:16:51 +0700

    feat(mobile): add ApiPatientRepository and ApiAdherenceRepository

---
+++ mobile/lib/data/repositories/api_patient_repository.dart
@@ -0,0 +1,29 @@
+import '../models/patient_model.dart';
+import 'patient_repository.dart';
+import '../../core/api_client.dart';
+
+class ApiPatientRepository implements IPatientRepository {
+  final ApiClient _client;
+
+  ApiPatientRepository(this._client);
+
+  @override
+  Future<List<Patient>> getMonitoringPatients() async {
+    final data = await _client.get('/patients') as List;
+    return data
+        .map((e) => Patient.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<Patient> getById(String id) async {
+    final data =
+        await _client.get('/patients/$id') as Map<String, dynamic>;
+    return Patient.fromJson(data);
+  }
+
+  Future<Patient> update(String id, Map<String, dynamic> body) async {
+    final data =
+        await _client.put('/patients/$id', body) as Map<String, dynamic>;
+    return Patient.fromJson(data);
+  }
+}
